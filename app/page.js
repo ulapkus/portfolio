0d60543bde9383fec997c127f8372b5d38fb659f
ulapkus <ulapkus@gmail.com>
2024-05-06 21:39:24 -0600

    added layers to scrolling

--- app/page.js
+++ app/page.js
@@ -1,113 +1,173 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState, useEffect, useRef } from "react";
+// import Link from "next/link";
+import arrow from "../public/images.png";
+import Image from "next/image";
+import browser from "../public/browser.png";
+import useScrollSnap from "react-use-scroll-snap";
+import backgroundd from "../public/backgroundd.png";
+
+const Navbar = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [zoomFactor, setZoomFactor] = useState(1);
+  const [showTint, setShowTint] = useState(false);
+  const [elementVisibility, setElementVisibility] = useState({
+    element1: false,
+    element2: false,
+    element3: false,
+    element4: false,
+    element5: false,
+  });
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Adjust these values to control the zoom behavior
+    const zoomInThreshold = 800; // Adjust this value to change when zooming starts
+    const maxZoomFactor = 1.3; // Adjust this value to change the maximum zoom level
+
+    let newZoomFactor = 1 + scrollPosition / zoomInThreshold;
+    newZoomFactor = Math.min(newZoomFactor, maxZoomFactor);
+
+    setZoomFactor(newZoomFactor);
+
+    // Show the tint when reaching the max zoom level
+    // if (newZoomFactor === maxZoomFactor) {
+    // setShowTint(true);
+    // } else {
+    //   setShowTint(false);
+    // }
+  }, [scrollPosition]);
+
+  useEffect(() => {
+    // Update visibility based on scroll position
+    if (scrollPosition >= 0 && scrollPosition < 100) {
+      setElementVisibility({
+        element1: true,
+        element2: false,
+        element3: false,
+        element4: false,
+        element5: false,
+      });
+    } else if (scrollPosition >= 100 && scrollPosition < 499) {
+      setElementVisibility({
+        element1: false,
+        element2: false,
+        element3: false,
+        element4: false,
+        element5: false,
+      });
+    } else if (scrollPosition >= 500 && scrollPosition < 1000) {
+      setElementVisibility({
+        element1: false,
+        element2: true,
+        element3: false,
+        element4: false,
+        element5: false,
+      });
+    } else if (scrollPosition >= 1000 && scrollPosition < 1500) {
+      setElementVisibility({
+        element1: false,
+        element2: true,
+        element3: false,
+        element4: false,
+        element5: false,
+      });
+      setShowTint(true);
+    } else if (scrollPosition >= 1500 && scrollPosition < 2000) {
+      setElementVisibility({
+        element1: false,
+        element2: false,
+        element3: false,
+        element4: false,
+        element5: false,
+      });
+    } else if (scrollPosition >= 500 && scrollPosition < 600) {
+      setElementVisibility({
+        element1: false,
+        element2: false,
+        element3: false,
+        element4: false,
+        element5: true,
+      });
+    } else {
+      setElementVisibility({
+        element1: false,
+        element2: false,
+        element3: false,
+        element4: false,
+        element5: false,
+      });
+    }
+  }, [scrollPosition]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      className="background"
+      // style={{ backgroundColor: bgColor }}
+      // ref={scrollRef}
+    >
+      {/* <Image src={backgroundd} /> */}
+
+      <div className="intro_border">
+        <div
+          className="intro_border_two"
+          style={{
+            backgroundImage: `url('https://i.ibb.co/TL08NVy/backgroundd.png')`,
+            backgroundSize: `${zoomFactor * 100}%`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            height: "100vh",
+            transform: `scale(${zoomFactor})`,
+            transition: "transform 0.5s ease", // Smooth transition for zoom effect
+          }}
+        >
+          <div
+            className={`element ${
+              elementVisibility.element1 ? "visible" : "hidden"
+            }`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {/* <div className={`content ${showTint ? "hidden" : ""}`}> */}
+            <div className="hi">Hello</div>
+            <div className="name">My name is Ula</div>
+          </div>
+
+          <div
+            className={`elementtwo ${
+              elementVisibility.element2 ? "visible" : "hidden"
+            }`}
+          >
+            {/* <div className="about-parent"> */}
+            {/* <div className="about"> */}
+            <div className="about_child">
+              I'm a self-taught web developer motivated by curiosity and
+              passion. I focus on building frontends in React, but I have
+              experience working across the entire stack.
+            </div>
+            {/* </div> */}
+          </div>
+          {/* </div> */}
+          {showTint && <div className="dark-tint" />}
+        </div>
+        <div className="projects">
+          <div className="word">hi!</div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="resume">
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Navbar;
