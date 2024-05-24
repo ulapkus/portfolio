01ffcf09fd1b78f837cf3818a1472b0fb36b4761
ulapkus <ulapkus@gmail.com>
2024-05-24 13:45:14 -0600

    changed mobile useeffect

--- app/page.js
+++ app/page.js
@@ -13,21 +13,36 @@ import Landscape from "./components_web/landscape";
 import Landscapemobile from "./components_mobile/landscape_mobile";
 
 export default function Portfolio() {
-  const [isMobile, setIsMobile] = useState(
-    window.matchMedia("(max-width: 768px)").matches
-  );
+  // const [isMobile, setIsMobile] = useState(
+  //   window.matchMedia("(max-width: 768px)").matches
+  // );
+
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 768px)");
-    const handleMediaChange = (e) => {
-      setIsMobile(e.matches);
-    };
-    setIsMobile(mediaQuery.matches);
-    mediaQuery.addEventListener("change", handleMediaChange);
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaChange);
-    };
+    if (typeof window !== "undefined") {
+      const mediaQuery = window.matchMedia("(max-width: 768px)");
+      const handleMediaChange = (e) => {
+        setIsMobile(e.matches);
+      };
+      setIsMobile(mediaQuery.matches);
+      mediaQuery.addEventListener("change", handleMediaChange);
+      return () => {
+        mediaQuery.removeEventListener("change", handleMediaChange);
+      };
+    }
   }, []);
+  // useEffect(() => {
+  //   const mediaQuery = window.matchMedia("(max-width: 768px)");
+  //   const handleMediaChange = (e) => {
+  //     setIsMobile(e.matches);
+  //   };
+  //   setIsMobile(mediaQuery.matches);
+  //   mediaQuery.addEventListener("change", handleMediaChange);
+  //   return () => {
+  //     mediaQuery.removeEventListener("change", handleMediaChange);
+  //   };
+  // }, []);
 
   return (
     <div className="main">
